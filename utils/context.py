1279fccced85d457776b05899bbc541ed2f1cb29
Willy <willychan@live.ca>
2020-12-17 15:59:44 -0500

    Change to custom tick

--- utils/context.py
+++ utils/context.py
@@ -66,8 +66,8 @@ class Context(commands.Context):
                 await prompt.delete()
 
     async def tick(self, value=True, reaction=True):
-        emojis = {True:  '<:greenTick:602811779835494410>',
-                  False: '<:redTick:602811779474522113>',
+        emojis = {True:  '<:tick:785940102353780736>',
+                  False: '<:cross:785940102542655539>',
                   None:  '<:greyTick:602811779810328596>'}
         emoji = emojis.get(value, '<:redTick:602811779474522113>')
         if reaction:
