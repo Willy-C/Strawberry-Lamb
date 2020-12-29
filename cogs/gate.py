cd1ed5674c325216472f9182657b07694cdb19e2
Willy <willychan@live.ca>
2020-12-29 18:16:11 -0500

    Handle MissingRequiredArgument error

--- cogs/gate.py
+++ cogs/gate.py
@@ -6,6 +6,7 @@ SUGGESTION_CHANNEL = 766154386694471681
 BOXOFFICE_CHANNEL = 763482832277995541
 SCHOOLGATE_CHANNEL = 758919988965801988
 RULES_CHANNEL = 777651693059178499
+MAIN_CHANNEL = 764197749247049758
 
 UPVOTE = '<:tick:785940102353780736>'
 DOWNVOTE = '<:cross:785940102542655539>'
@@ -47,6 +48,19 @@ class SchoolGate(commands.Cog):
         if channel is None:
             return
         await channel.send(f'{member.mention} has transferred away from {member.guild}, we’ll miss you!')
+
+    @commands.Cog.listener()
+    async def on_member_update(self, before, after):
+        if before.guild.id != GUILD:
+            return
+
+        if not before._roles.has(758919988965801985) and after._roles.has(758919988965801985):
+            channel = self.bot.get_channel(MAIN_CHANNEL)
+            if channel:
+                await channel.send(f'Welcome to Strawberry Lamb, {before.mention}! '
+                                   f'If you’d like to customize your roles or pick your academies make sure to head to '
+                                   f'<#763216417939652609> or <#763163080783167518> '
+                                   f'to introduce yourself <:hee:781698624714309652>')
 
 
 def setup(bot):
